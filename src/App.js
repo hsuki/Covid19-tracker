4627002a7f9f35a9e5c30256e5a5905b264e73c3
Jon <jondwoo@pop-os.localdomain>
2020-08-09 17:02:01 -0700

    fetch daily data

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import classes from './App.module.css';
 
 import Cards from './Cards/Cards';
 import Chart from './Chart/Chart';
-import { fetchNationalData } from './api';
+import { fetchCurrentUS, fetchDailyUS } from './api';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: {} };
-  }
+  state = {
+    currentData: {},
+    dailyData: [],
+  };
 
   async componentDidMount() {
-    const {
-      data: [{ positive, recovered, death, lastModified }],
-    } = await fetchNationalData();
-    this.setState({ data: { positive, recovered, death, lastModified } });
+    const { currentData } = await fetchCurrentUS();
+    const dailyData = await fetchDailyUS();
+    this.setState({
+      currentData: {
+        positive: currentData.positive,
+        recovered: currentData.recovered,
+        death: currentData.death,
+        lastModified: currentData.lastModified,
+      },
+      dailyData: dailyData,
+    });
   }
 
   render() {
-    const currentData = this.state.data;
+    const currentData = this.state.currentData;
+    const dailyData = this.state.dailyData;
     return (
       <div className={classes.Container}>
         <Cards currentData={currentData} />
-        <Chart />
+        <Chart dailyData={dailyData} />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
 
-export const fetchNationalData = async () => {
+export const fetchCurrentUS = async () => {
   const url = 'https://api.covidtracking.com/v1/us/current.json';
   try {
-    const data = await axios.get(url);
+    const {
+      data: [{ positive, recovered, death, lastModified }],
+    } = await axios.get(url);
 
-    return data;
+    return { currentData: { positive, recovered, death, lastModified } };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchDailyUS = async () => {
+  const url = 'https://api.covidtracking.com/v1/us/daily.json';
+  try {
+    const { data } = await axios.get(url);
+    return data.map((dailyData) => ({
+      positive: dailyData.positive,
+      recovered: dailyData.recovered,
+      death: dailyData.death,
+      lastModified: dailyData.lastModified,
+    }));
   } catch (error) {
     console.log(error);
   }
